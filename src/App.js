300514c8e17becc84cd40dc6948c744022970cf0
Tara <32590508+talyke@users.noreply.github.com>
2022-01-31 00:00:04 -0700

    swapi wip
    json parse star wars data react app with api hooks

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
+import React from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    let [responseData, setResponseData] = React.useState('');
+    const myList = document.querySelector('ul');
+    const myRequest = new Request('names.json');
+
+    const fetchData = React.useCallback(() => {
+        axios({
+            "method": "GET",
+            "url": "https://swapi.dev/api/people/",
+            "headers": {
+                "content-type": "application/json",
+            }, "params": {
+                "language_code": "en",
+                "results": "[{name}]",
+            }
+        })
+            .then((response) => {
+                setResponseData(response.data)
+            })
+        fetch(myRequest)
+            .then(response => response.json())
+            .then(data => {
+                for (const name of data.names) {
+                    let listItem = document.createElement('li');
+                    listItem.appendChild(
+                        document.createElement('strong')
+                    ).textContent = name.Name;
+                    listItem.append(
+                        ` born in homeworld planet ${name.Name}. Name: `
+                    );
+                    listItem.appendChild(
+                        document.createElement('strong')
+                    ).textContent = `${name.planet}`;
+                    myList.appendChild(listItem);
+                }
+            })
+            .catch(console.error);
+           
+    }, [])
+
+    React.useEffect(() => {
+        fetchData()
+    }, [fetchData])
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>
+                    Fetching Data with React Hooks
+                </h1>
+                <button type='button' onClick={fetchData}>Click for Data</button>
+            </header>
+            <main>
+                {responseData &&
+                    <blockquote>
+                        "{responseData && responseData.content}"
+                        <small>{responseData && responseData.originator && responseData.originator.name}</small>
+                    </blockquote>
+                }
+            </main>
+            {/* <pre>
+        <code>
+          {responseData && JSON.stringify(responseData, null, 4)}
+        </code>
+      </pre> */}
+        </div>
+    );
 }
 
 export default App;
